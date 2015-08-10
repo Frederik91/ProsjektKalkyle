a67c5c324a920f6260f5445c37003047792fd5d7
Frederik Tegnander <frederik.t91@gmail.com>
2015-08-10 15:13:12 +0200

    Første layout

---
+++ Prosjektkalkyle/ViewModels/KalkyleViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prosjektkalkyle.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Prosjektkalkyle.ViewModels
+{
+    class KalkyleViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentViewModel;
+
+        public KalkyleViewModel()
+        {
+            ProjectInfoCommand = new DelegateCommand(o => OpenProjectInfoWindow());
+        }
+
+        public void OpenProjectInfoWindow()
+        {
+            CurrentViewModel = new ProjectInfoViewModel();
+        }
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                this.OnPropertyChanged("CurrentViewModel");
+            }
+        }
+
+
+        public ICommand ProjectInfoCommand { get; private set; }
+
+    }
+}
---
+++ Prosjektkalkyle/ViewModels/ProjectInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prosjektkalkyle.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Prosjektkalkyle.ViewModels
+{
+    class ProjectInfoViewModel : ViewModelBase
+    {
+
+        public ProjectInfoViewModel()
+        {
+            MessageBox.Show("lol");
+        }
+    }
+}
